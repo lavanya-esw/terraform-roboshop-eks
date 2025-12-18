b11c985f6612996ad3aaefbdcd45bd0714f7dd6d
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-12-18 22:47:43 +0530

    K8 resources

---
+++ 00-vpc/main.tf
@@ -0,0 +1,11 @@
+module "vpc"{
+    source = "git::https://github.com/lavanya-esw/terraform_aws_vpc.git?ref=main"
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project
+    environment = var.env
+    vpc_tags = local.vpc_tags
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+    is_peering_required = true
+}
