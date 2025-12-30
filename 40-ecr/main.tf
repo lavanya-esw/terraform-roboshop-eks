c4a1b9094b256d65a429fde7b9a971a139b2c43b
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-12-30 21:39:55 +0530

    jenkins shared pipeline

--- 40-ecr/main.tf
+++ 40-ecr/main.tf
@@ -1,5 +1,43 @@
-resource "aws_ecr_repository" "foo" {
+resource "aws_ecr_repository" "main" {
   name                 = "${var.project}/catalogue"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project}/cart"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project}/user"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project}/payment"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.project}/shipping"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
