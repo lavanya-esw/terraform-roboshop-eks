a8be18111f0161137caeaf63e9db94408b064f43
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-12-30 13:01:12 +0530

    jenkins pipeline

---
+++ 40-ecr/main.tf
@@ -0,0 +1,9 @@
+resource "aws_ecr_repository" "foo" {
+  name                 = "${var.project}/catalogue"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  force_delete = true
+}
